511c04bbc7f06213c03d41cf8213af72332f88d7
SREEHARI S <39848434+sreehari50@users.noreply.github.com>
2018-08-03 20:34:20 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,33 @@
+import numpy as np
+graph = {    'Vazhakulam': ['Avoly','Nadukara','Ayavana'],
+             'Nadukara': ['Arakuzha','Vazhakulam'],
+             'Arakuzha': ['Perumballoor','Nadukara'],
+             'Perumballoor': ['Arakuzha'],
+             'Avoly': ['Vazhakulam'],
+             'Ayavana': ['Anchapetty Jn','Vazhakulam','Varappetty'],
+             'Anchapetty Jn': ['Ayavana','Puthuppadi','Anicadu'],
+             'Puthuppadi': ['Karukadam','Chalikkadavu','Anchapetty Jn'],
+             'Varappetty': ['Karukadam','Mathirappilly','Kothamangalam','Ayavana'],
+             'Karukadam': ['Mathirappilly','varappetty','Puthuppadi'],
+             'Mathirappilly': ['Karukadam','Varapetty','Kothamangalam'],
+             'Kothamangalam': ['Mathirappilly','Varappetty'],
+             'Anicadu': ['Chalikkadavu','Anchapetty Jn'],
+             'Chalikkadavu': ['Kanam','Puthuppadi','Anicadu'],
+             'Kizhakkekara': ['Kanam'],
+             'Kanam':['Chalikkadavu']    
+         }
+def find_shortest_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not start in graph:
+            return None
+        shortest = None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_shortest_path(graph, node, end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+print(find_shortest_path(graph, 'Avoly', 'Puthuppadi'))
